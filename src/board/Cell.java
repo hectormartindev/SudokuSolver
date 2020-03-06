f403d074911f6a124b77ed0b574520aa163932f1
Héctor Martín Armas <61806551+hectormartindev@users.noreply.github.com>
2020-03-06 00:00:26 +0000

    updated the project with CLI
    updated the CLI to make it more userfriendly

--- src/board/Cell.java
+++ src/board/Cell.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package board;
 
 public class Cell {
@@ -24,30 +23,3 @@ public class Cell {
 		number = n;
 	}
 }
-=======
-package board;
-
-public class Cell {
-
-	/*Attributes*/
-	private Number number;
-	
-	/*Builders*/
-	public Cell() {
-		number = Number.BLANK;
-	}
-	
-	public Cell(Number n) {
-		number = n;
-	}
-	
-	/*Methods*/
-	public Number getNumber() {
-		return number;
-	}
-	
-	public void setNumber(Number n) {
-		number = n;
-	}
-}
->>>>>>> aebadff9e481aa6232d3ae52b8bdd37ccad5e774
--- src/board/Number.java
+++ src/board/Number.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package board;
 
 public enum Number {
@@ -13,19 +12,3 @@ public enum Number {
 	EIGHT,
 	NINE
 }
-=======
-package board;
-
-public enum Number {
-	BLANK,
-	ONE,
-	TWO,
-	THREE,
-	FOUR,
-	FIVE,
-	SIX,
-	SEVEN,
-	EIGHT,
-	NINE
-}
->>>>>>> aebadff9e481aa6232d3ae52b8bdd37ccad5e774
